5b55058c870d86b3fa8a9c683a5ac2943112f36a
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-04 18:55:38 +0530

    Add a naive way to load plugin from name passed

--- src/plugin.c
+++ src/plugin.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "plugin.h"
 #include "datatypes.h"
 
 int plugin_load(const char *plugin_name)
 {
-    void *handle = dlopen("synergy/plugins/get_notif.so", RTLD_NOW);
+    char *plugin_path;
+    char *function_name;
+
+    /* compute the plugin path */
+    plugin_path = (char *) malloc(sizeof("synergy/plugins/.so") + sizeof(plugin_name));
+    strcpy(plugin_path, "synergy/plugins/");
+    strcat(plugin_path, plugin_name);
+    strcat(plugin_path, ".so");
+    printf("%s\n", plugin_path);
+
+    /* open the plugin */
+    void *handle = dlopen(plugin_path, RTLD_NOW);
     if (!handle) {
         printf("Fuck\n");
         return 1;
     }
-    init_func_t plugin_init_func = (init_func_t) (__intptr_t) dlsym(handle, "plugin_get_notif_init");
-    plugin_init_func();
+
+    /* compute plugin init function path */
+    function_name = (char *) malloc(sizeof("plugin_init_") + sizeof(plugin_name));
+    strcpy(function_name, "plugin_");
+    strcat(function_name, plugin_name);
+    strcat(function_name, "_init");
+
+    /* get plugin init function address */
+    plugin_init_func_t plugin_init_func = (plugin_init_func_t) (__intptr_t)
+        dlsym(handle, function_name);
+
+    /* run the plugin init function */
+    (*plugin_init_func)();
+
     printf("Loaded plugins: %s\n", plugin_name);
     return 1;
 }
