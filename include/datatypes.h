6bbabfb9cc22a41890663717c5facbad7e2d8961
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-05 02:24:15 +0530

    Complete event emit and attach functions

--- include/datatypes.h
+++ include/datatypes.h
@@ -9,12 +9,13 @@ enum message_type {
     MT_PAIR,
 };
 
-enum event_type {
+typedef enum event_type {
     ET_DEVICE_NEW,
     ET_DEVICE_PAIR,
     ET_NOTIFICATION,
     ET_COMMAND,
-};
+    ET_SIZE
+} event_type_t;
 
 /* TODO: optimize this data struct */
 typedef struct message_struct {
@@ -28,6 +29,8 @@ typedef int (*plugin_init_func_t)(void);
 typedef struct event_struct {
     enum event_type type;
 } event_t;
+
+typedef void (*event_handler_t)(void);
     
 
 #endif /* end of include guard: DATATYPES_H */
--- src/event.c
+++ src/event.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "datatypes.h"
 #include "event.h"
 
-void event_emit(enum event_type event_type)
+struct event_item {
+    event_handler_t handler;
+    struct event_item *next;
+};
+
+struct event_item_list {
+    struct event_item *top;
+    struct event_item *last;
+} event_list[ET_SIZE];
+
+
+void event_emit(event_type_t event_type)
 {
-    printf("emitted event: %d\n", event_type);
+    struct event_item *item = event_list[event_type].top;
+
+    /* TODO: make it async */
+    while (item != NULL) {
+        (*(item -> handler))();
+        item = item -> next;
+    }
+}
+
+void event_attach(event_type_t event_type, event_handler_t event_handler)
+{
+    struct event_item *new_event_item =
+        (struct event_item *) malloc(sizeof(struct event_item));
+    new_event_item -> next = NULL;
+    new_event_item -> handler = event_handler;
+
+    if (event_list[event_type].top == NULL) {
+        event_list[event_type].top = new_event_item;
+        event_list[event_type].last = new_event_item;
+    } else {
+        event_list[event_type].last -> next = new_event_item;
+        event_list[event_type].last = new_event_item;
+    }
 }
